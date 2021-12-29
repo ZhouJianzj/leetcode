c26ef38087ced72974ca9a7659855c242f59f4f4
ZhouJianzj <2998390669@qq.com>
2021-12-29 23:51:58 +0800

    集合的复习

--- case/src/main/java/tTest/Seven.java
+++ case/src/main/java/tTest/Seven.java
@@ -1,24 +1,31 @@
 package tTest;
 
+import javax.annotation.Resource;
 import java.io.*;
 
 public class Seven {
     public static void main(String[] args) throws IOException {
+        System.out.println(countNum(readFile(), "hello"));
+    }
+
+    public static StringBuilder readFile()throws IOException{
         FileReader fileReader = new FileReader(new File("D:\\b.txt"));
         char[] chars = new char[10];
         StringBuilder stringBuilder = new StringBuilder(1024);
         int a;
-        int count = 0;
+
         while ( (a = fileReader.read(chars)) != -1){
             String s = new String(chars, 0, a);
             stringBuilder.append(s);
-            System.out.println(s);
         }
+        return stringBuilder;
+    }
+    public static int countNum(StringBuilder stringBuilder,String s){
+        int count = 0;
         int b = -1;
-        while ((b = stringBuilder.indexOf("hello",++b)) != -1){
+        while ((b = stringBuilder.indexOf(s,++b)) != -1){
             ++count;
         }
-        System.out.println(count);
+        return count;
     }
-
 }
